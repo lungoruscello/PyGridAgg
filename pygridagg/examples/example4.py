101f30cfde41bfbe1c8724a254761c54feaf8e14
lungoruscello <saschal@ethz.ch>
2025-04-04 18:37:43 +0200

    Add example on custom subclassing

---
+++ pygridagg/examples/example4.py
@@ -0,0 +1,48 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from aggregate import BasePointAggregator
+from aggregate import SquareGridLayout
+from examples import load_japanese_earthquake_data
+
+
+# Implement a custom aggregator via subclassing
+
+class CustomThresholdCounter(BasePointAggregator):
+    """Counts the number of points whose weight is above a threshold."""
+
+    def aggregate(self, point_weights, threshold):
+        # Initialise grid counts with zeroes. `self.layout.shape`
+        # gives the grid size in terms of (rows, columns).
+        counts = np.full(self.layout.shape, fill_value=0, dtype=int)
+
+        # Select the column and row indexes of eligible points.
+        # `self.inside_mask` is Boolean mask coding for points
+        # inside the grid bounds.
+        point_mask = self.inside_mask & (point_weights > threshold)
+        col_ids = self.grid_col_ids[point_mask]
+        row_ids = self.grid_row_ids[point_mask]
+
+        # Use `np.add.at` for fast in-place addition
+        np.add.at(counts, (row_ids, col_ids), 1)  # noqa
+
+        return counts
+
+
+# Load example data on earthquakes around Japan
+quake_coords, magnitudes = load_japanese_earthquake_data()
+
+# Define a square grid layout with 2,500 cells, encompassing all
+# earthquake locations
+layout = SquareGridLayout.from_points(quake_coords, num_cells=2_500)
+
+# Quickly count earthquakes above magnitude 6 across grid cells
+thresh = 6
+agg = CustomThresholdCounter(layout, quake_coords, point_weights=magnitudes, threshold=thresh)
+
+# Check that no earthquakes were 'lost'
+assert agg.cell_aggregates.sum() == (magnitudes > thresh).sum()
+
+# Plot the aggregated data as a heatmap
+ax = agg.plot()
+plt.show()
