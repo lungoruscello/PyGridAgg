96db0d5cc83e4c76d94597bd125c68efc6f4c3dc
lungoruscello <saschal@ethz.ch>
2025-04-07 18:16:03 +0200

    Add `setup.py`

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+import pygridagg as agg
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setup(
+    name="PyGridAgg",
+    version=agg.__version__,
+    description=agg.__about__,
+    url=agg.__url__,
+    author=agg.__author__,
+    license=agg.__license__,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=['numpy'],
+    extras_require={
+        "plotting": ["matplotlib"],
+        "dev": ["pytest>=7.0", "twine>=4.0.2"],
+    },
+    classifiers=["Programming Language :: Python :: 3",
+                 "License :: OSI Approved :: MIT License",
+                 "Operating System :: OS Independent"],
+    python_requires='>=3.6',
+)
